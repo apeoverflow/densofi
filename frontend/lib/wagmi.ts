1050adc30ca0450755585cc6985c2cb7c0aa6a0e
kieran williams <kieranmarcus@gmail.com>
2025-06-18 21:24:19 +0100

    updates to wallet adapter

--- frontend/lib/wagmi.ts
+++ frontend/lib/wagmi.ts
@@ -3,18 +3,19 @@ import { mainnet, sepolia, flowMainnet, flowTestnet, celo, celoAlfajores, worldc
 import { http } from 'wagmi';
 
 export const wagmiConfig = getDefaultConfig({
-  appName: 'Simple Storage App',
-  projectId: '0e011a7dafb96ed999ec1f44c5824370',
-  chains: [sepolia, mainnet, flowMainnet, flowTestnet, celo, celoAlfajores, worldchain, worldchainSepolia],
+  appName: 'Densofi',
+  projectId: "ff3f43403a454a58b7132ab2977ab740",
+  chains: [flowMainnet, sepolia],
+    //  mainnet, , flowTestnet, celo, celoAlfajores, worldchain, worldchainSepolia],
   transports: { 
-    [sepolia.id]: http('https://sepolia.infura.io/v3/f63336cd46ea40d68f1577991e1135cf'), 
-    [mainnet.id]: http('https://mainnet.infura.io/v3/f63336cd46ea40d68f1577991e1135cf'), 
     [flowMainnet.id]: http(), 
-    [flowTestnet.id]: http(), 
-    [celo.id]: http(), 
-    [celoAlfajores.id]: http(), 
-    [worldchain.id]: http(), 
-    [worldchainSepolia.id]: http(), 
+    [sepolia.id]: http('https://sepolia.infura.io/v3/f63336cd46ea40d68f1577991e1135cf'), 
+    // [flowTestnet.id]: http(), 
+    // [mainnet.id]: http('https://mainnet.infura.io/v3/f63336cd46ea40d68f1577991e1135cf'), 
+    // [celo.id]: http(), 
+    // [celoAlfajores.id]: http(), 
+    // [worldchain.id]: http(), 
+    // [worldchainSepolia.id]: http(), 
   },
   // Disable SSR for Wagmi to avoid hydration issues
   ssr: false,
